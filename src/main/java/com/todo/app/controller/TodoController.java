3e583e0ecf66132421b357c7deb76ed40cd43436
宮村 <ks.miya1229@icloud.com>
2024-11-29 14:25:24 +0900

    更新機能実装

--- src/main/java/com/todo/app/controller/TodoController.java
+++ src/main/java/com/todo/app/controller/TodoController.java
@@ -28,4 +28,10 @@ public class TodoController {
 		todoMapper.add(todo);
 		return "redirect:/";
 	}
+	
+	@RequestMapping(value="/update")
+	public String update(Todo todo) {
+		todoMapper.update(todo);
+		return "redirect:";
+	}
 }
--- src/main/java/com/todo/app/mapper/TodoMapper.java
+++ src/main/java/com/todo/app/mapper/TodoMapper.java
@@ -11,4 +11,6 @@ public interface TodoMapper {
 	public List<Todo> selectAll();
 	
 	public void add(Todo todo);
+	
+	public void update(Todo todo);
 }
